abf1b5f5783ccfb3564c87e7404a03c6c2a7bcc2
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-04-29 18:09:32 +0200

    flatten root element in exported json

--- editem_apparatus/apparatus_converter.py
+++ editem_apparatus/apparatus_converter.py
@@ -5,6 +5,8 @@ import xml.etree.ElementTree as ET
 import xmltodict
 from loguru import logger
 
+ns = {'xml': 'http://www.w3.org/XML/1998/namespace'}
+
 
 class ApparatusConverter:
     def __init__(self, apparatus_directory: str, output_directory: str):
@@ -28,7 +30,7 @@ class ApparatusConverter:
 
         # export json conversion of complete xml file
         xpars = xmltodict.parse(xml)
-        js = json.dumps(xpars, indent=2, ensure_ascii=False)
+        js = json.dumps(list(xpars.values())[0], indent=2, ensure_ascii=False)
         path = f"{output_dir}/{base_name}.json"
         logger.info(f"=> {path}")
         with open(path, 'w', encoding='UTF8') as f:
@@ -36,13 +38,12 @@ class ApparatusConverter:
 
         # export all elements with xml:id to json files
         root = ET.fromstring(xml)
-        ns = {'xml': 'http://www.w3.org/XML/1998/namespace'}
         identified_elements = root.findall(".//*[@xml:id]", namespaces=ns)
         for element in identified_elements:
-            xml_id = element.attrib.get('{http://www.w3.org/XML/1998/namespace}id')
+            xml_id = element.attrib.get(f'{{{ns["xml"]}}}id')
             xml_str = ET.tostring(element, encoding='unicode')
             parsed_dict = xmltodict.parse(xml_str)
             filepath = os.path.join(output_dir, f"{xml_id}.json")
             logger.info(f"=> {filepath}")
             with open(filepath, 'w', encoding='utf-8') as f:
-                json.dump(parsed_dict, f, indent=2, ensure_ascii=False)
+                json.dump(list(parsed_dict.values())[0], f, indent=2, ensure_ascii=False)
