f19102672b9ba57b246a9c4c8d8178aaec721912
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-04-30 14:34:07 +0200

    fix test

--- test/apparatus_test.py
+++ test/apparatus_test.py
@@ -1,11 +1,18 @@
 import unittest
 
 from editem_apparatus.apparatus_converter import ApparatusConverter
+from editem_apparatus.editem_apparatus_config import EditemApparatusConfig
 
 
 class ApparatusTestCase(unittest.TestCase):
     def test_converter(self):
-        ac = ApparatusConverter("data/test-apparatus/", "out/test")
+        cf = EditemApparatusConfig(
+            project_name="test",
+            data_path="data/test-apparatus/",
+            static_file_server_base_url="http://localhost:8080",
+            export_path="out/test"
+        )
+        ac = ApparatusConverter(cf)
         self.assertIsNotNone(ac)
 
 
